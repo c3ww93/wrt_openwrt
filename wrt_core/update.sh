dfb5338a4ea0ec6edeef58be72815f64cbe0d73e
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2026-09-15 10:15:04 +0800

    Update update.sh
    Signed-off-by: yxs123321 <53416197+yxs123321@users.noreply.github.com>

--- wrt_core/update.sh
+++ wrt_core/update.sh
@@ -25,7 +25,7 @@ fi
 FEEDS_CONF="feeds.conf.default"
 GOLANG_REPO="https://github.com/sbwml/packages_lang_golang"
 GOLANG_BRANCH="26.x"
-LAN_ADDR="192.168.1.1"
+LAN_ADDR="192.168.68.1"
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 BASE_PATH=${BASE_PATH:-$SCRIPT_DIR}
